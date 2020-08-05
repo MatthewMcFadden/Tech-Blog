bbd345b1ebccce6c41d0775cadce859fff62da83
Matt M <matt.mcfadden14@gmail.com>
2020-08-04 23:07:55 -0600

    bcrypt added to code

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 // GET
 router.get('/', (req, res) => {
   // Access our User model and run .findAll() method)
-  User.findAll()
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -15,6 +17,7 @@ router.get('/', (req, res) => {
 // GET
 router.get('/:id', (req, res) => {
   User.findOne({
+    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
     }
@@ -50,6 +53,7 @@ router.post('/', (req, res) => {
 // PUT
 router.put('/:id', (req, res) => {
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
